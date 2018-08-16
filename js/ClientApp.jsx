3167d3c84578b8f4e707ce8307c0ad49ba16bc2a
Kartik <kartik.arora@1mg.com>
2018-08-16 13:13:13 +0530

    v3-1

---
+++ js/ClientApp.jsx
@@ -0,0 +1,32 @@
+const ce = React.createElement;
+
+const MyTitle = props => {
+  return ce(
+    "div",
+    null,
+    ce(
+      "h1",
+      {
+        style: {
+          color: props.color
+        }
+      },
+      props.title
+    )
+  );
+};
+
+const myFirstComponent = () => {
+  return ce(
+    "div",
+    {
+      id: "my-first-component"
+    },
+    ce(MyTitle, { title: "Game of Thrones", color: "YellowGreen" }),
+    ce(MyTitle, { title: "Stranger Things", color: "GreenYellow" }),
+    ce(MyTitle, { title: "Rick and Morty", color: "LimeGreen" }),
+    ce(MyTitle, { title: "Silicon Valley", color: "Peru" })
+  );
+};
+
+ReactDOM.render(ce(myFirstComponent), document.getElementById("app"));
